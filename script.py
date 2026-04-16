116d2b1cc30ea00523750953eb36fd1470015904
jesse <jessevandis@gmail.com>
2026-04-16 16:07:38 +0200

    Fixed scene4

--- script.py
+++ script.py
@@ -16,7 +16,7 @@ def getenv_int(name, default=0):
     except ValueError:
         return default
 
-debug_scene="scene3"
+debug_scene="scene4"
 
 _should_render = False
 RENDERING_ENABLED = os.getenv("RENDERING_ENABLED", "False") == "False"
